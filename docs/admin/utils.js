72796e6e5872248fb4b96ee148594f36d3eb82ed
nam145127 <van145127@gmail.com>
2025-07-04 13:09:25 +0700

    Update utils.js

--- docs/admin/utils.js
+++ docs/admin/utils.js
@@ -21,12 +21,12 @@ export function formatDate(dateString) {
 
 export function checkAdminAccess() {
   const token = localStorage.getItem('token');
-  if (!token) return location.href = '';
+  if (!token) return location.href = 'https://nam145127.github.io/foodshop/user/login.html';
 }
 
 document.addEventListener('click', e => {
   if (e.target.id === 'logoutBtn') {
     localStorage.removeItem('token');
-    location.href = '';
+    location.href = 'https://nam145127.github.io/foodshop/user/login.html';
   }
 });
